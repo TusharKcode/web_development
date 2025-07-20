721397d6fac9fe95055083bb6d300ded09d8afc5
Tushar Kumar <tkgoyal104@gmail.com>
2025-07-20 17:28:20 +0530

    JavaScript Started

---
+++ javaScript.js
@@ -0,0 +1,32 @@
+//--------------------------------------------VARIABLES
+
+let fullName = "Tushar";
+const bithYear = 2003;
+let currentYear = 2025;
+let age = 23;
+
+console.log("Name: ", fullName)
+console.log("Age: ", age)
+
+//--------------------------------------------DATA TYPES
+
+let Uname = "Tushar";                        //---- String
+let score = 98.5;                           //---- Numbers
+let isPassed = true;                        //---- Boolean
+let colors = ["red", "blue", "green"];      //---- Array
+let user = {                                //---- Object
+    name: "Tushar",
+    age: 24,
+    isAdmin: false
+};
+
+let emptyValue = null;                      //---- Null
+let notAssigned;                            //---- Undefined
+
+console.log("Name: ", Uname);
+console.log("Score: ", score);
+console.log("Is Passed? ", isPassed);
+console.log("Colors: ", colors);
+console.log("User Object: ", user);
+console.log("Null: ", emptyValue);
+console.log("Undefined", notAssigned);
